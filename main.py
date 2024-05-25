ccfb765fd305e2c63b607efaf898edfa5713decf
Aleksander Popov <apopov@azul.com>
2024-05-25 15:03:03 +0200

    Add tournament registration mode

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
+import os
 import argparse
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, CallbackQueryHandler, CallbackContext
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+)
+
 from utils.spreadsheet_utils import SpreadsheetUtils
-from utils.schedule_io_utils import ScheduleIOUtils
+from utils.users_database_utils import UsersDatabaseUtils
 
+CONFIG = {}
 
-SPREADSHEET_ID = ""
-KEYFILE = ""
+def read_config():
+    file_path = 'config.txt'
+    if not os.path.exists(file_path):
+        print("Config does not exist.")
+        return False
+
+    try:
+        with open(file_path, 'r') as file:
+            for line in file.readlines():
+                key, value = line.strip().split('=')
+                CONFIG[key] = value
+    except Exception as e:
+        print(f"Error reading config: {e}")
+        return False
+    return True
 
 def read_table(keyfile_path, spreadsheet_id):
     try:
@@ -15,44 +35,89 @@ def read_table(keyfile_path, spreadsheet_id):
         worksheet = spreadsheet.get_worksheet(0)  # For the first worksheet
         scheduler = ScheduleIOUtils(worksheet)
         return scheduler.get_playoff_schedule()
-    except FileNotFoundError as e:
-        return f"Error: Keyfile not found at {keyfile_path}. Exception: {e}"
-    except PermissionError as e:
-        return f"Error: Permission denied when accessing keyfile at {keyfile_path}. Exception: {e}"
-    except ConnectionError as e:
-        return f"Error: Failed to connect to the spreadsheet with ID {spreadsheet_id}. Exception: {e}"
     except Exception as e:
-        return f"An unexpected error occurred: {e}"
+        return f"Error: {e}"
 
-def read_args():
-    parser = argparse.ArgumentParser(description='Telegram Bot Options for FC Mobile League Tournament')
-    parser.add_argument('--spreadsheet_id', required=True, help='The ID of the Google Spreadsheet')
-    parser.add_argument('--bot_token', required=True, help='The ID of the Telegram Bot Token')
-    parser.add_argument('--key_file', required=True, help='The ID of the json keyfile')
-    args = parser.parse_args()
-    return args
+def register_user_respond(user_id, username):
+    if username is None:
+        return "Братишка, установи username в Телеге, пожалуйста :)"
+
+    db = UsersDatabaseUtils()
+    if not db.connect(CONFIG.get('key_path'), CONFIG.get('users_db')):
+        return "Failed to connect to the database."
+
+    row_id = db.get_row_by_user_id(user_id)
+    if row_id is not None:
+        if db.get_user_info(user_id, 'part') == '1':
+            return f"{username}, ты УЖЕ зарегистрирован!"
+
+    db.add_user(user_id, username)
+    db.set_user_info(user_id, 'part', '1')
+    return f"Спасибо за регистрацию в турнире, {username}! Удачи!"
+
+def participants_respond():
+    db = UsersDatabaseUtils()
+    if not db.connect(CONFIG.get('key_path'), CONFIG.get('users_db')):
+        return "Failed to connect to the database."
+
+    participants = db.get_participants_list()
+    sorted_participants = sorted(participants, key=lambda x: x.rate, reverse=True)
+
+    result = "Участники их рейтинг в РИ:\n\n"
+    for i, participant in enumerate(sorted_participants, start=1):
+        result += f"{i}. {participant.username} [{participant.rate}]\n"
+    return result
+
+def set_user_rating_respond(user_id, username, rating):
+    if username is None:
+        return "Братишка, установи username в Телеге, пожалуйста :)"
+
+    db = UsersDatabaseUtils()
+    if not db.connect(CONFIG.get('key_path'), CONFIG.get('users_db')):
+        return "Failed to connect to the database."
+
+    db.add_user(user_id, username)
+    try:
+        rating = int(rating)
+        if db.set_user_info(user_id, 'fcrate', rating):
+            return f"Рейтинг обновлен: {rating}"
+        else:
+            return "Failed to update rating"
+    except ValueError:
+        return "Рейтинг должен быть целым числом!"
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.message.from_user
-    print(f'You talk with user {user["username"]} and his user ID: {user["id"]}')
-    res = read_table(KEYFILE, SPREADSHEET_ID)
-    await update.message.reply_text(res)
+    print(f'[start] You talk with user {user["username"]} and his user ID: {user["id"]}')
+    await update.message.reply_text(participants_respond())
 
+async def register_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.message.from_user
+    print(f'[register] You talk with user {user["username"]} and his user ID: {user["id"]}')
+    await update.message.reply_text(register_user_respond(user["id"], user["username"]))
+
+async def set_rating_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.message.from_user
+    print(f'[set_rating] You talk with user {user["username"]} and his user ID: {user["id"]}')
+    if len(context.args) != 1:
+        await update.message.reply_text("Необходимо указать свой рейтинг, напрмер: '/setrating 123'")
+        return
+    await update.message.reply_text(set_user_rating_respond(user["id"], user["username"], context.args[0]))
 
 def init_bot(token):
     print("Starting bot...")
     application = Application.builder().token(token).build()
-    application.add_handler(CommandHandler("showresults", start_command))
+    application.add_handler(CommandHandler("showstatus", start_command))
+    application.add_handler(CommandHandler("register", register_command))
+    application.add_handler(CommandHandler("setrating", set_rating_command))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
-
 def main():
-    args = read_args()
-    global SPREADSHEET_ID
-    global KEYFILE
-    SPREADSHEET_ID = args.spreadsheet_id
-    KEYFILE = args.key_file
-    init_bot(args.bot_token)
+    if not read_config():
+        print("Failed to read the config.")
+        return
+    print(CONFIG)
+    init_bot(CONFIG.get('bot_token'))
 
 if __name__ == "__main__":
     main()
